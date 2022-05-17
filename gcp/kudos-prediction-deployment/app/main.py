1e74fd6bf3d69c061c7ba0c3bb6b69b9df9b1733
Ty Andrews <ty.andrews@fisherpaykel.com>
2022-05-17 12:10:26 +1200

    Made make file for deploying kudos-prediction

--- gcp/kudos-prediction-deployment/app/main.py
+++ gcp/kudos-prediction-deployment/app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import List
+import logging
 import uvicorn
 
 from . import mlib
+
+logging.basicConfig(level=logging.INFO)
 
 description = """
 Stravasnooper API Helps You Understand Your Strava Data! 🚀
@@ -31,28 +36,62 @@ async def root():
     intro_message = """<h1><span style="color: #ff6600;">Welcome to the Stravasnooper ML API</span></h1> 
                     <p>This is a collection of models/tools developed from a completely unnecessary exploration of my Strava data.</p> 
                     <p><strong>To learn how to use this API for your own projects or interest visit: 
-                    <a href="https://ml.stravasnooper.com/">ml.stravasnooper.com/docs</a></strong></p>"""
+                    <a href="https://ml.stravasnooper.com/docs">ml.stravasnooper.com/docs</a></strong></p>"""
     return HTMLResponse(content=intro_message, status_code=200)
 
 
-@app.get("/kudos-prediction")
+class PredictedKudos(BaseModel):
+    perc_follwers: List[float]
+    num_kudos: List[int]
+
+
+@app.get("/kudos-prediction", response_model=PredictedKudos)
 async def predict_kudos(
-    custom_name: int,
-    distance: int,
-    achievements: int,
-    elevation: int,
-    followers: int,
+    custom_name: List[int] = Query(
+        default=None,
+        ge=0,
+        le=1,
+        description="Provide 0 for standard Strava name, 1 for custom name.",
+    ),
+    distance: List[int] = Query(
+        default=None, ge=10, le=250, description="Ride distance in km's."
+    ),
+    achievements: List[int] = Query(
+        default=None, ge=0, le=60, description="No. of achievements on the ride."
+    ),
+    elevation: List[int] = Query(
+        default=None, ge=0, le=3000, description="Elevation gain in m's."
+    ),
+    followers: int = Query(
+        default=None, ge=10, le=100000, description="Current number of followers."
+    ),
 ):
 
-    prediction = mlib.predict(
-        custom_name_bool=custom_name,
-        achievement_count=achievements,
-        total_elevation_gain=elevation,
-        distance_km=distance,
-        num_followers=followers,
-    )
+    # check each pass has matching number of inputs
+    if len(set(map(len, [custom_name, distance, achievements, elevation]))) != 1:
+        raise HTTPException(
+            status_code=404,
+            detail="Number of values passed for parameters do not match.",
+        )
+    else:
 
-    return prediction
+        perc_followers, num_kudos = mlib.predict(
+            custom_name_bool=custom_name,
+            achievement_count=achievements,
+            total_elevation_gain=elevation,
+            distance_km=distance,
+            num_followers=followers,
+        )
+
+        predict_result = {
+            "perc_followers": perc_followers,
+            "num_kudos": num_kudos,
+        }
+
+        logging.debug(f"Prediction: {predict_result}")
+        print(predict_result)
+
+        return predict_result
 
 
 if __name__ == "__main__":
