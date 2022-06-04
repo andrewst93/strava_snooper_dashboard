9a7aedeafd64946b2d20114977e2cc2f0840dd05
Ty Andrews <ty.andrews@fisherpaykel.com>
2022-06-05 04:16:26 +1200

    sphinx docs build setup and rst generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ release = "0.1"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "index.py", "app.py"]
+exclude_patterns = ["_build", "launch.py", "app.py"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
